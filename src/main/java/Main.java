1ca29ee33fbc1299d3255ed8b72c762924b7ca95
Idkirsch <s195483@student.dtu.dk>
2020-01-09 10:53:58 +0100

    test1

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import gui_main.GUI;
+
+
+
+public class Main {
+
+
+
+
+    public static void main(String[] args) {
+         GUI gui = new GUI();
+         gui.setDie(6);
+         gui.getUserButtonPressed();
+         gui.setDie(5);
+    }
+}
+
